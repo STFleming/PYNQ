3c92c8bc24d3aaea3113851e88f599bbd7337e11
Shane T. Fleming <shane.t.fleming@gmail.com>
2022-06-28 16:03:13 +0000

    Structure of the hierarchy dict view almost there

--- pynq/metadata/hierarchy_dict_view.py
+++ pynq/metadata/hierarchy_dict_view.py
@@ -1,10 +1,12 @@
 from typing import Dict, List
 
 import json
-from pynqmetadata import Module, ProcSysCore, SubordinatePort
+from pynqmetadata import Module, ProcSysCore, SubordinatePort, Hierarchy
 from pynqmetadata.errors import FeatureNotYetImplemented
 
 from .append_drivers_pass import DriverExtension
+from .ip_dict_view import IpDictView
+from .mem_dict_view import MemDictView
 
 def _default_repr(obj):
     return repr(obj)
@@ -16,28 +18,58 @@ class HierarchyDictView:
 
     def __init__(self, module: Module) -> None:
         self._md = module
+        self._ip_dict = IpDictView(self._md)
+        self._mem_dict = MemDictView(self._md)
 
-    def _build_hierarchy(self, hier:Dict, new_items:List[str])->None:
-        """ Recursively walks down hier adding items in new_items """
-        if new_items[0] not in hier:
-            hier[new_items[0]] = {}
+    def _hierarchy_walker(self, r:Dict, h:Hierarchy)->None:
+        """ recursive walk down the hierarchy h, adding IP
+        that is a match in ip_dict or mem_dict to the hierarchies
+        """
+        if h.name not in r:
+            r[h.name] = {}
+            r[h.name]["ip"] = {}
+            r[h.name]["memories"] = {}
+            r[h.name]["hierarchies"] = {}
 
-        if len(new_items) > 1:
-            next_items = new_items[1:]
-            self._build_hierarchy(hier[new_items[0]], next_items)
+        for ip in h.core_obj.values():
+            if ip.hierarchy_name in self._ip_dict: 
+                r[h.name]["ip"][ip.name] = ip.ref
+            if ip.hierarchy_name in self._mem_dict:
+                r[h.name]["memories"][ip.name] = ip.ref
+
+        for hier in h.hierarchies_obj.values():
+            self._hierarchy_walker(r[h.name]["hierarchies"], hier)
+
+    def _prune_walker(self, r:Dict)->bool:
+        """ Walks down through the hierarchy dict, removing anything
+        that is empty """
+        for item in r.values():
+            empty = False
+
+            if "hierarchies" in item:
+                del_list = []
+                for i,h in item["hierarchies"].items():
+                    if self._prune_walker(h):
+                        empty = True
+                        del_list.append(i)
+
+                for i in del_list:
+                    del item["hierarchies"][i]
+
+                return len(item["ip"]) == 0 and len(item["memories"]) == 0
+            return True
 
     @property
     def hierarchy_dict(self) -> Dict:
+        """
+        Walks down the hierarchy dict and whenever it encounters an IP
+        that is in the ip_dict or mem_dict keep it.
+        """
         repr_dict = {}
-        for core in self._md.cores.values():
-            add_core = False
-            for port in core.ports.values():
-                if isinstance(port, SubordinatePort):
-                    if len(port.registers)>1:
-                        add_core = True
-            if add_core:
-                hier = core.hierarchy_name.split("/")
-                self._build_hierarchy(repr_dict, hier)
+        top_level = self._md.hierarchies
+        for hierarchy in top_level.hierarchies_obj.values():
+            self._hierarchy_walker(repr_dict, hierarchy)      
+            self._prune_walker(repr_dict)
 
         return repr_dict
 
